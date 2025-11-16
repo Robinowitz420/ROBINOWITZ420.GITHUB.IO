203c30efe6b79259d69cb07dde6f996c42fef40f
Robin <robinrussellfrench@gmail.com>
2025-11-16 10:53:36 -0500

    Add next.config.js for GitHub Pages deployment

---
+++ next.confi.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Makes the app export as static HTML/CSS/JS
+  basePath: '/ROBINOWITZ420.GITHUB.IO', // Must match your repository name exactly
+  assetPrefix: '/ROBINOWITZ420.GITHUB.IO/',
+};
+
+module.exports = nextConfig;
